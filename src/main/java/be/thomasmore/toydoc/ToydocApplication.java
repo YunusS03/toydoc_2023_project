61fc0fa0c1dbb078806811e660faa14edbabe2d6
YunusPC01 <r0930908@student.thomasmore.be>
2023-04-20 13:47:22 +0100

    opstarten

---
+++ src/main/java/be/thomasmore/toydoc/ToydocApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.toydoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToydocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToydocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/thomasmore/toydoc/ToydocApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.toydoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToydocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
