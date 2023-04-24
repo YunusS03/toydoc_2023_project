d9dc35addec96a8ba1ac8619de72673eab002d74
HiveTyrand <janssensrobin@live.be>
2023-04-24 10:37:28 +0200

    added AppointmentController

--- src/main/java/be/thomasmore/toydoc/controllers/AppointmentController.java
+++ src/main/java/be/thomasmore/toydoc/controllers/AppointmentController.java
@@ -1,4 +1,12 @@
 package be.thomasmore.toydoc.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AppointmentController {
+    @GetMapping("/appointment")
+    public String appointment() {
+        return "appointment";
+    }
 }
