c9539f1aeab29286542ec5ddc4231babd8e29684
YunusPC01 <r0930908@student.thomasmore.be>
2023-04-27 19:54:05 +0100

    Calendar next previous

--- src/main/java/be/thomasmore/toydoc/controllers/CalendarController.java
+++ src/main/java/be/thomasmore/toydoc/controllers/CalendarController.java
@@ -3,16 +3,18 @@ package be.thomasmore.toydoc.controllers;
 import be.thomasmore.toydoc.model.CalendarService;
 import be.thomasmore.toydoc.model.Day;
 import be.thomasmore.toydoc.model.TimeSlot;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
 public class CalendarController {
     private CalendarService calendarService;
+
 
     public CalendarController(CalendarService calendarService) {
         this.calendarService = calendarService;
@@ -23,17 +25,40 @@ public class CalendarController {
         CalendarService calendarService = new CalendarService();
         List<Day> calendar = calendarService.getCurrentWeek();
         model.addAttribute("calendar", calendar);
-
         return "test1";
     }
 
-        @PostMapping("/next-week")
-        public String nextWeek(Model model) {
-            CalendarService calendarService = new CalendarService();
-            List<Day> calendar = calendarService.getNextWeek();
-            model.addAttribute("calendar", calendar);
-            return "test1";
-        }
+
+
+    private int weekCount = 0;
+    @PostMapping("/nextWeek")
+    public String nextWeek(Model model, @RequestParam(value = "count", defaultValue = "0") int count) {
+        count++;
+        weekCount = count;
+        System.out.println("next"+ count);
+        model.addAttribute("count", count);
+        CalendarService calendarService = new CalendarService();
+        List<Day> calendar = calendarService.getNextWeek(weekCount);
+        model.addAttribute("calendar", calendar);
+        return "test1";
+    }
+
+    @PostMapping("/previousWeek")
+    public String prevWeek(Model model, @RequestParam(value = "count", defaultValue = "0") int count) {
+        weekCount = count;
+        count--;
+        System.out.println("prev "+count);
+        model.addAttribute("count", count);
+        CalendarService calendarService = new CalendarService();
+        List<Day> calendar = calendarService.getPreviousWeek(weekCount);
+        model.addAttribute("calendar", calendar);
+        return "test1";
+    }
+
+
+
+
+
 
 }
 
--- src/main/java/be/thomasmore/toydoc/model/CalendarService.java
+++ src/main/java/be/thomasmore/toydoc/model/CalendarService.java
@@ -14,64 +14,79 @@ public class CalendarService {
     private int numHoursInDay = 24;
     private int numDaysInMonth = 30; // Change this to the number of days in the desired month
     private int numDaysInWeek = 7; // Change this to the number of days in the desired month
-
-    private LocalDate currentDate;
+    private LocalDate dateOfArray;
+    List<Day> calendar = new ArrayList<>();
 
 
     public CalendarService() {
-        currentDate = LocalDate.now();
-
-
+        dateOfArray = LocalDate.now();
     }
+
+//    public void setOccupiedOnCalender(Date date, TimeSlot hour, Calendar calendar){
+//        for (Calendar cal: calendar) {
+//
+//
+//        }
+//
+//
+//
+//    }
 
 
     public List<Day> getCurrentWeek() {
-        List<Day> calendar = new ArrayList<>();
-        Calendar calendarInstance = Calendar.getInstance();
-        calendarInstance.set(Calendar.HOUR_OF_DAY, 0);
-        calendarInstance.set(Calendar.MINUTE, 0);
-        calendarInstance.set(Calendar.SECOND, 0);
-        calendarInstance.set(Calendar.MILLISECOND, 0);
 
-        LocalDate currentDate = LocalDate.now().plusDays(1);
-        LocalDateTime currentDateTime = LocalDateTime.of(currentDate, LocalTime.now());
 
-        int currentHour = currentDateTime.getHour();
+        LocalTime currentTime = LocalTime.now();
+        int currentHour = currentTime.getHour();
 
         for (int i = 0; i < 7; i++) {
 
-            Date date = calendarInstance.getTime();
+            LocalDate date0 = dateOfArray;
+
+            ZonedDateTime zonedDateTime = date0.atStartOfDay(ZoneId.systemDefault());
+            Date date = Date.from(zonedDateTime.toInstant());
+
             Day day = new Day(numHoursInDay, date);
 
-            Instant instant = day.getDate().toInstant();
-            LocalDate localDate = instant.atZone(ZoneId.systemDefault()).toLocalDate();
-
             // If the current day is before today, set all hours to occupied
-            if (localDate.isBefore(currentDate)) {
+            if (dateOfArray.isBefore(LocalDate.now())) {
                 for (TimeSlot hour : day.getHours()) {
                     hour.setOccupied(true);
                 }
             }
             // If the current day is today, set all past hours to occupied
-            else if (localDate.isEqual(currentDate)) {
+            else if (dateOfArray.isEqual(LocalDate.now())) {
                 for (TimeSlot hour : day.getHours()) {
                     if (hour.getHour() < currentHour) {
                         hour.setOccupied(true);
                     }
                 }
             }
-
             calendar.add(day);
-            calendarInstance.add(Calendar.DAY_OF_MONTH, 1);
+           dateOfArray = dateOfArray.plusDays(1);
         }
         return calendar;
     }
 
 
 
-    public List<Day> getNextWeek() {
-        currentDate = currentDate.plusWeeks(1);
+
+    public List<Day> getNextWeek(int plusweek) {
+        dateOfArray = dateOfArray.plusWeeks(plusweek);
+        System.out.println(""+ dateOfArray);
+        System.out.println(plusweek);
         return getCurrentWeek();
     }
+
+    public List<Day> getPreviousWeek(int plusweek) {
+        dateOfArray = dateOfArray.plusWeeks(plusweek);
+        System.out.println(""+ dateOfArray);
+        return getCurrentWeek();
+    }
+
+
+
+
+
 }
 
