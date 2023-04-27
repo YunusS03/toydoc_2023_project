4ecc41cbd87e4797018690489774fbcaa8ce5ab9
YunusS03 <r0930908@student.thomasmore.be>
2023-04-27 11:10:07 +0200

    extra info , 1 client 1 docotor

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,20 @@
-
--- wachtwoorden zijn voor de dummys altijd  "test"  dummy1 dummy2 dummy3
+-- Inserts for app_user
 INSERT INTO app_user (dtype, first_name, last_name, password, role, username, address, age, city, country, email, phone, postal_code, speciality)
-VALUES ('doctor', 'John', 'Doe', '$2a$12$NzQP7G0NTWFwaUjRZgf09.IHgc5gRq17BzGZREa9/n1DUI9v.fTNG', 3, 'dummy1', '123 Main St', 30, 'New York', 'USA', 'john.doe@gmail.com', '555-123-4567', '10001', 'IT Specialist');
+VALUES ('client', 'John', 'Doe', '$2a$12$NzQP7G0NTWFwaUjRZgf09.IHgc5gRq17BzGZREa9/n1DUI9v.fTNG', 1, 'dummy1', '123 Main St', 30, 'New York', 'USA', 'john.doe@email.com', '555-123-4567', '10001', NULL);
 
-INSERT INTO app_user (dtype, first_name, last_name, password, role, username, address, age, city, country, email, phone, postal_code)
-VALUES ('client', 'Jane', 'Smith', '$2a$12$NzQP7G0NTWFwaUjRZgf09.IHgc5gRq17BzGZREa9/n1DUI9v.fTNG', 4, 'dummy2', '456 Elm St', 25, 'Los Angeles', 'USA', 'jane.smith@gmail.com', '555-987-6543', '90001');
+INSERT INTO app_user (dtype, first_name, last_name, password, role, username, address, age, city, country, email, phone, postal_code, speciality)
+VALUES ('doctor', 'Jane', 'Smith', '$2a$12$NzQP7G0NTWFwaUjRZgf09.IHgc5gRq17BzGZREa9/n1DUI9v.fTNG', 2, 'dummy2', '456 Second St', 40, 'Los Angeles', 'USA', 'jane.smith@email.com', '555-987-6543', '90001', 'Pediatrics');
 
-INSERT INTO app_user (dtype, first_name, last_name, password, role, username, address, age, city, country, email, phone, postal_code)
-VALUES ('client', 'Bob', 'Johnson', '$2a$12$NzQP7G0NTWFwaUjRZgf09.IHgc5gRq17BzGZREa9/n1DUI9v.fTNG', 4, 'dummy3', '789 Oak St', 40, 'Chicago', 'USA', 'bob.johnson@gmail.com', '555-555-5555', '60601');
+-- Inserts for toy
+INSERT INTO toy (name, type, appointment_id, client_id)
+VALUES ('Teddy Bear', 'Stuffed Animal', NULL, 1);
 
--- !!merk op!! dtype verwijst naar de classen client en doctor of app_user en we hebben ook een role enum wat niet meer klopt ik laat het voor nu zo aangezien dit nu vlekkenloos werkt.
+INSERT INTO toy (name, type, appointment_id, client_id)
+VALUES ('Lego Set', 'Building Blocks', NULL, 1);
+
+-- Inserts for appointment
+INSERT INTO appointment (date, time, client_id, doctor_id, toy_id)
+VALUES ('2023-05-01', '09:00:00', 1, 2, 1);
+
+INSERT INTO appointment (date, time, client_id, doctor_id, toy_id)
+VALUES ('2023-05-02', '14:00:00', 1, 2, 2);
