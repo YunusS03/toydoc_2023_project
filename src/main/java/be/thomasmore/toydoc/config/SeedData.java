00dfc0ca25e2a32aeb99ab1bc3452471afff1f3e
Justine Dor <118171057+Jusss10@users.noreply.github.com>
2023-05-03 21:30:07 +0200

    Made a Component

---
+++ src/main/java/be/thomasmore/toydoc/config/SeedData.java
@@ -0,0 +1,29 @@
+package be.thomasmore.toydoc.config;
+
+import be.thomasmore.toydoc.model.Post;
+import be.thomasmore.toydoc.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class SeedData implements CommandLineRunner {
+
+    @Autowired
+    private PostService postService;
+
+    @Override
+    public void run(String... args) throws Exception{
+        List<Post> posts = postService.getAll();
+
+        if(posts.size() == 0){
+            Post post1 = new Post();
+            post1.setTitle("title of post 1");
+            post1.setBody("This is the body of post 1");
+
+            postService.save(post1);
+        }
+    }
+}
