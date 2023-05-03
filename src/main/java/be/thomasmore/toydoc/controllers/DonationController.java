d6f85116967a3544581c6d2bff4a22dd97f76be5
ahmetnuriuygun <ahmetnuriuygun1@gmail.com>
2023-05-03 12:42:00 +0200

    Er worden nieuwe donation objects aan de database kunnen toevoegen.

--- src/main/java/be/thomasmore/toydoc/controllers/DonationController.java
+++ src/main/java/be/thomasmore/toydoc/controllers/DonationController.java
@@ -1,33 +1,41 @@
 package be.thomasmore.toydoc.controllers;
 
 import be.thomasmore.toydoc.model.Donation;
-import be.thomasmore.toydoc.repositories.DonateRepository;
+import be.thomasmore.toydoc.repositories.DonationRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 @Controller
 public class DonationController {
 
     private Logger logger = LoggerFactory.getLogger(DonationController.class);
+    @Autowired
 
-    private DonateRepository donateRepository;
+    private DonationRepository donationRepository;
 
     private String localPlan;
     private Integer localAmount;
+
     private String localFirstName;
     private String localLastName;
+
+    private String localTitle;
     private String localEmail;
+
+    private String localPhoneNumber;
+
+    private String localCardHolder;
+
+    private String localExpiry;
+
+    private String localCvv;
     private String localSecurityCardNumber;
 
-//    @ModelAttribute("donation")
-//    public Donation findParty(@PathVariable(required = false) Integer id) {
-//        return new Donation();
-//    }
+
 
     @GetMapping("/donation")
     public String donation(Model model){
@@ -58,35 +66,57 @@ public class DonationController {
 
     @GetMapping("donation/info")
     public String donationInfoForm(Model model,@RequestParam(required=false)String title,String firstName,String lastName,String email,String phoneNumber){
+        localTitle = title;
         localFirstName = firstName;
         localLastName = lastName ;
         localEmail = email;
+        localPhoneNumber = phoneNumber;
+
         model.addAttribute("openCheckout",true);
         return "donation";
     }
     @GetMapping("donation/checkout")
     public String donationCheckoutForm(Model model,@RequestParam(required=false)String cardHolder,String cardNumber,String expiry,String cvv){
+        localCardHolder = cardHolder;
+        localExpiry = expiry;
+        localCvv = cvv;
+
         String securityCardNumber = "*******" + cardNumber.substring(12,15) ;
         localSecurityCardNumber = securityCardNumber;
         model.addAttribute("openSave",true);
-        model.addAttribute("plan",localPlan);
-        model.addAttribute("amount",localAmount);
-        model.addAttribute("firstName",localFirstName);
-        model.addAttribute("lastName",localLastName);
-        model.addAttribute("email",localEmail);
-        model.addAttribute("securityCardNumber",localSecurityCardNumber);
+        model.addAttribute("localPlan",localPlan);
+        model.addAttribute("localAmount",localAmount);
+        model.addAttribute("localFirstName",localFirstName);
+        model.addAttribute("localLastName",localLastName);
+        model.addAttribute("localEmail",localEmail);
+        model.addAttribute("localSecurityCardNumber",localSecurityCardNumber);
         return "donation";
     }
 
-//    @GetMapping("donation/save")
-//        public String donationSave(Model model){
-//        return "donation";
-//    }
-//
-//    @PostMapping("/donation/save")
-//    public String newDonate(Model model,Donation donation) {
-//
-//        donateRepository.save(donation);
-//        return "donation";
-//    }
+    @ModelAttribute("donation")
+    public Donation findDonation(@PathVariable(required = false) Integer id) {
+        return new Donation();
+    }
+
+    @GetMapping("donation/save")
+        public String donationSave(Model model){
+        return "donation";
+    }
+
+    @PostMapping("donation/save")
+    public String newDonate(Model model, Donation donation) {
+        donation.setPlan(localPlan);
+        donation.setAmount(localAmount);
+        donation.setTitle(localTitle);
+        donation.setFirstName(localFirstName);
+        donation.setLastName(localLastName);
+        donation.setEmail(localEmail);
+        donation.setPhoneNumber(localPhoneNumber);
+        donation.setCardHolder(localCardHolder);
+        donation.setExpiry(localExpiry);
+        donation.setCvv(localCvv);
+        donation.setCardNumber(localSecurityCardNumber);
+        donationRepository.save(donation);
+        return "donation";
+    }
 }
--- src/main/java/be/thomasmore/toydoc/repositories/DonationRepository.java
+++ src/main/java/be/thomasmore/toydoc/repositories/DonationRepository.java
@@ -1,8 +1,7 @@
 package be.thomasmore.toydoc.repositories;
 
 import be.thomasmore.toydoc.model.Donation;
-import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.data.repository.CrudRepository;
 
-public interface DonateRepository extends CrudRepository<Donation, Integer> {
+public interface DonationRepository extends CrudRepository<Donation, Integer> {
 }
