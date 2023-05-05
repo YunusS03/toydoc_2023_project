3e0063840421e7786eac266fe50b56c93ed580f4
YunusPC01 <r0930908@student.thomasmore.be>
2023-05-05 21:38:31 +0100

    First Design and Try

---
+++ src/main/resources/static/JS/toys.js
@@ -0,0 +1,26 @@
+var bearImage = document.createElement('img');
+
+bearImage.src = 'https://cdn-icons-png.flaticon.com/512/2174/2174053.png';
+
+bearImage.style.position = 'absolute';
+bearImage.style.top = '0';
+bearImage.style.left = '0';
+
+var divElement = document.getElementById('your-div-id');
+divElement.appendChild(bearImage);
+
+
+// Create bear image element
+var bearImage = document.createElement('img');
+bearImage.src = 'https://cdn-icons-png.flaticon.com/512/2174/2174053.png';
+bearImage.style.position = 'absolute';
+bearImage.style.top = '0';
+bearImage.style.left = '0';
+
+// Append bear image to div element
+var divElement = document.getElementById('your-div-id');
+divElement.appendChild(bearImage);
+
+// Repeat for each div
+// var divElement2 = document.getElementById('your-div-id-2');
+// divElement2.appendChild(bearImage);
