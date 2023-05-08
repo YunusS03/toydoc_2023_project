319de73fbc11233bb469501db795ae442fed672e
YunusPC01 <r0930908@student.thomasmore.be>
2023-05-08 23:47:43 +0100

    Test controller voor testing purposes , ook voor de reminder

---
+++ src/main/java/be/thomasmore/toydoc/controllers/TestingController.java
@@ -0,0 +1,26 @@
+package be.thomasmore.toydoc.controllers;
+
+
+import be.thomasmore.toydoc.scheduler.AppointmentReminder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestingController {
+
+
+    private final AppointmentReminder appointmentReminder;
+
+
+    @Autowired
+    public TestingController(AppointmentReminder appointmentReminder) {
+        this.appointmentReminder = appointmentReminder;
+    }
+
+    @GetMapping("/test-reminders")
+    public String testReminders() {
+        appointmentReminder.sendAppointmentReminders();
+        return "test/test_reminders"; // Return the appropriate view
+    }
+}
