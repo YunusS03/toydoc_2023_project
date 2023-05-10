ca6ce23441d98acdab6f51bd53483d052737994e
HiveTyrand <janssensrobin@live.be>
2023-05-10 22:01:00 +0200

    file upload system. Tested-by: Robin. Working with files up to 1MB NOT COMPLETED

--- src/main/java/be/thomasmore/toydoc/controllers/UploadController.java
+++ src/main/java/be/thomasmore/toydoc/controllers/UploadController.java
@@ -3,26 +3,35 @@ package be.thomasmore.toydoc.controllers;
 import be.thomasmore.toydoc.service.GoogleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
 
 @Controller
 public class UploadController {
     @Autowired
     private GoogleService googleService;
 
+    public UploadController(GoogleService googleService) {
+        this.googleService = googleService;
+    }
+
+
     @GetMapping("/upload")
-    public String upload(@RequestParam(required = false) MultipartFile image) {
-        File pom = new File("./pom.xml");
-        try {
-            String url = googleService.toFirebase(pom, "pom.xml");
-            System.out.println("File Upload Succesful at:"+url);
-        } catch (Exception ex) {
-            System.out.println("Niet gelukt " + ex.getMessage());
-        }
+    public String uploadForm(Model model, MultipartFile file) throws IOException {
+        model.addAttribute("file", file);
         return "upload";
+    }
+
+    @PostMapping("/uploadsubmit")
+    public String uploadSubmit(@RequestParam("file") MultipartFile file, Model model) throws IOException {
+        googleService.uploadFile(file);
+        return "redirect:upload";
     }
 }
--- src/main/java/be/thomasmore/toydoc/service/GoogleService.java
+++ src/main/java/be/thomasmore/toydoc/service/GoogleService.java
@@ -2,20 +2,20 @@ package be.thomasmore.toydoc.service;
 
 import com.google.auth.Credentials;
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.storage.BlobId;
-import com.google.cloud.storage.BlobInfo;
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageOptions;
+import com.google.cloud.storage.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -39,5 +39,34 @@ public class GoogleService {
         storage.create(blobInfo, Files.readAllBytes(file.toPath()));
         return String.format("https://firebasestorage.googleapis.com/v0/b/tm-speelgoeddokter.appspot.com/o/%s?alt=media", URLEncoder.encode(fileName, StandardCharsets.UTF_8));
     }
+
+    public void uploadFile(MultipartFile file) throws IOException {
+
+        // Create a storage client
+        StorageOptions options = StorageOptions.newBuilder()
+                .setCredentials(GoogleCredentials.fromStream(new FileInputStream(jsonFile)))
+                .build();
+        Storage storage = options.getService();
+
+        // Get the input stream from the uploaded file
+        InputStream inputStream = file.getInputStream();
+
+        // Define the file name and destination path
+        String fileName = file.getOriginalFilename();
+        String destinationPath = "uploads/" + fileName;
+
+        // Define the blob info
+        BlobInfo blobInfo = BlobInfo.newBuilder(BlobId.of(imageBucket, destinationPath))
+                .setContentType(file.getContentType())
+                .build();
+
+        // Upload the file to Firebase Storage
+        Blob blob = storage.create(blobInfo, inputStream);
+
+        // Close the input stream
+        inputStream.close();
+    }
+
+
 
 }
