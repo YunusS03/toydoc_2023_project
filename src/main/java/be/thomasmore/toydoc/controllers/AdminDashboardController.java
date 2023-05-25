7fe62d7810bd0147337b54f842770a70b1d880fe
YunusPC01 <r0930908@student.thomasmore.be>
2023-05-25 18:23:00 +0100

    Admin DashBoard Add Doc / remove Doc -->login + CustomUserNameGenerate

--- src/main/java/be/thomasmore/toydoc/controllers/AdminDashboardController.java
+++ src/main/java/be/thomasmore/toydoc/controllers/AdminDashboardController.java
@@ -25,16 +25,17 @@ import java.util.Optional;
 @RequestMapping("/admin")
 public class AdminDashboardController {
 
-  @Autowired
-  private AppUserRepository appUserRepository;
+    @Autowired
+    private AppUserRepository appUserRepository;
 
-  @Autowired
-  private AppointmentRepository appointmentRepository;
+    @Autowired
+    private AppointmentRepository appointmentRepository;
 
-  @Autowired
-  private PostRepository postRepository;
+    @Autowired
+    private PostRepository postRepository;
+
     @GetMapping("/dashboard")
-    public String admin(Model model){
+    public String admin(Model model) {
 
         List<AppUser> listOfAllDoctors = appUserRepository.findByRoleList(Role.DOCTOR);
         List<AppUser> listOfAllClients = appUserRepository.findByRoleList(Role.CLIENT);
@@ -43,112 +44,82 @@ public class AdminDashboardController {
         System.out.println("heeey");
 
 
-
-        model.addAttribute("amountOfDoctors",listOfAllDoctors.size());
-        model.addAttribute("amountOfClients",listOfAllClients.size());
-        model.addAttribute("amountOfAppointments",listOfAllAppointments.size());
-        model.addAttribute("amountOfPosts",listOfAllPost.size());
+        model.addAttribute("amountOfDoctors", listOfAllDoctors.size());
+        model.addAttribute("amountOfClients", listOfAllClients.size());
+        model.addAttribute("amountOfAppointments", listOfAllAppointments.size());
+        model.addAttribute("amountOfPosts", listOfAllPost.size());
 
         return "admin/dashboard";
     }
 
-    @GetMapping({"/searchDoctor","/searchDoctor/{filter}"})
-    public String searchDoctor(Model model, @RequestParam(required = false)String keyword) {
-        List<AppUser> doctors ;
-        if(keyword==null){
+    @GetMapping({"/searchDoctor", "/searchDoctor/{filter}"})
+    public String searchDoctor(Model model, @RequestParam(required = false) String keyword) {
+        List<AppUser> doctors;
+        if (keyword == null) {
             doctors = appUserRepository.findByRoleList(Role.DOCTOR);
-        }else{
-            doctors = appUserRepository.findByFirstNameContainingIgnoreCase(keyword,Role.DOCTOR);
+        } else {
+            doctors = appUserRepository.findByFirstNameContainingIgnoreCase(keyword, Role.DOCTOR);
 
         }
-        model.addAttribute("doctors",doctors);
+        model.addAttribute("doctors", doctors);
 
         return "admin/searchDoctor";
     }
 
-    @GetMapping({"/deleteDoctor","/deleteDoctor/{filter}"})
-    public String deleteDoctor(Model model, @RequestParam(required = false)String keyword) {
-        List<AppUser> doctors ;
-        if(keyword==null){
+    @GetMapping({"/deleteDoctor", "/deleteDoctor/{filter}"})
+    public String deleteDoctor(Model model, @RequestParam(required = false) String keyword) {
+        List<AppUser> doctors;
+        if (keyword == null) {
             doctors = appUserRepository.findByRoleList(Role.DOCTOR);
-        }else{
-            doctors = appUserRepository.findByFirstNameContainingIgnoreCase(keyword,Role.DOCTOR);
+        } else {
+            doctors = appUserRepository.findByFirstNameContainingIgnoreCase(keyword, Role.DOCTOR);
 
         }
-        model.addAttribute("doctors",doctors);
+        model.addAttribute("doctors", doctors);
 
         return "admin/deleteDoctor";
     }
 
 
-
-    @PostMapping("/deleteDoctor/{id}")
+    @PostMapping("/removeDoctor/{id}")
     public String userDelete(@PathVariable int id, HttpServletRequest request, HttpServletResponse response) {
-//        System.out.println("isdeleted");
-//
-
-//        SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
-//        logoutHandler.setInvalidateHttpSession(true);  //alles wordt geinvalideerd dat de gbr tot dan in de cache heeft
-//        logoutHandler.logout(request, response, SecurityContextHolder.getContext().getAuthentication());
 
         Optional<AppUser> doctor = appUserRepository.findById(id);
-        List<Appointment> appointments =  appointmentRepository.findByDoctor(doctor.get());
+        List<Appointment> appointments = appointmentRepository.findByDoctor(doctor.get());
+        for (Appointment a : appointments) {
+            a.setDoctor(null);
+        }
 
-//        List<Toy> toysClient =  toyRepository.findByClient(appUser);
-//        List<Toy> toysAppointment = null;
-
-        System.out.println("==============[][]DELETING USER ALL INFOS[][]=================");
-
-        System.out.println("========[] AppUser Client - TOYS []=========");
-//        for (Toy a: toysClient) {
-//            System.out.println("Deleting TOY from id "  +  a.getClient().getId() +"wtih name toy : "+ a.getName());
-//            toyRepository.deleteById(a.getId());
-//        }
-//        toyRepository.deleteAll(toysClient);
-
-        System.out.println("==============[] Appoinments []=================");
-//        for (Appointment a: appointments
-//             ) {
-//            System.out.println("Deleting APPOINTMENTS Doctor= "  +  a.getDoctor().getId() +  " client= "  + a.getClient().getId()  +  " Date= "  + a.getDate());
-//            toysAppointment = toyRepository.findByAppointment(a);
-//        }
-
-//        toyRepository.deleteAll(toysAppointment);
-
-        System.out.println("========[] Appoinments - TOYS []=========");
-//        for (Toy toy: toysAppointment) {
-//            System.out.println(toy.getId());
-//            toyRepository.deleteById(toy.getId());
-//        }
-
-//        toyRepository.deleteAll(toysAppointment);
-
-        System.out.println("==============[][]DELETING USER ALL INFOS[][]=================");
-
-        appointmentRepository.deleteAll(appointments);
+        appointmentRepository.saveAll(appointments);
         appUserRepository.delete(doctor.get());
 
-        return "redirect:admin/dashboard";
+        return "redirect:/admin/dashboard";
     }
-
-
 
 
     @GetMapping("/addDoctor")
     public String addDoctor(Model model) {
+        AppUser appUsertest = new AppUser();
+        appUsertest.setUsername("appuser" + appUserRepository.count());
+        model.addAttribute("appUser0", appUsertest);
         return "admin/addDoctor";
     }
 
     @PostMapping("/addDoctor")
-    public String addDoctorPost(Model model, AppUser appUser) {
+    public String addDoctorPost(@ModelAttribute("appUser") AppUser appUserNew, Model model) {
+        
+        //write me a check if username already exists
+        
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        appUser.setPassword(encoder.encode(appUser.getPassword()));
-        appUser.setRole(Role.DOCTOR);
-        appUserRepository.save(appUser);
-        return "admin/dashboard";
+        appUserNew.setPassword(encoder.encode(appUserNew.getPassword()));
+        appUserNew.setRole(Role.DOCTOR);
+        appUserRepository.save(appUserNew);
+
+        // Add the appUser object to the model
+        model.addAttribute("appUser0", appUserNew);
+
+        return "redirect:/admin/dashboard";
     }
-
-
 
 
 }
